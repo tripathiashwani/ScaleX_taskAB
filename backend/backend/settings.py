03f0ffd50b1fe926bcfb965c8a4fc0069957ccbe
ASHWANI TRIPATHI <tashwani475@gmail.com>
2024-03-23 00:33:43 +0530

    Task B done

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -97,6 +97,7 @@ DATABASES = {
 
 
 
+
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
 
--- taskB.py
+++ taskB.py
@@ -1,53 +1,42 @@
-from django.http import JsonResponse
-from rest_framework.decorators import api_view
-from rest_framework.views import APIView
-
 import pymongo
+from pymongo import MongoClient
+import simplejson as json
+
+
 import json
-import pymongo
-import json
-from bson import ObjectId 
 
-
-
-client = pymongo.MongoClient("mongodb+srv://tashwani475:j56dRmajznRHWYV3@cluster0.4kmsi7u.mongodb.net/")
+# Load JSON data
+client = MongoClient("mongodb+srv://tashwani475:CcoOwcyNE8OJGdhE@cluster1.vxzjw6p.mongodb.net/")
 db = client['newDB1']
-collection = db['sample']
+collection = db['sample2']  # Change this to your collection name
 
-cursor = collection.find()
-data = list(cursor)
-for item in data:
-    item['_id'] = str(item['_id'])
-json_string = json.dumps(data)
-
-data = json.loads(json_string)
-num_pairs = len(data[0]["pairs"])
-
-print("Number of pairs:", num_pairs)
-print(data[0]["schemaVersion"])
-y=data[0]["pairs"][0]["baseToken"]["symbol"]
-print(data[0]["pairs"][0]["baseToken"]["symbol"])
+from pymongo import MongoClient
 
 
+# 1. Create (Insert)
+new_entry = {
+    "pair": {
+        "name": "New Pair",
+        "pairAddress": "new_pair_address",
+        "baseToken": {"name": "Base Token", "symbol": "BASE"},
+        "quoteToken": {"name": "Quote Token", "symbol": "QUOTE"},
+        "price": {"usd": 10.0},
+        "volume": {"24hours": 1000},
+        "liquidity": {"usd": 500},
+        "createdAt": 1710685033000
+    }
+}
+insert_result = collection.insert_one(new_entry)
+print("Inserted ID:", insert_result.inserted_id)
 
+# 2. Read (Retrieve)
+print("Data after insertion:")
+for entry in collection.find():
+    print(entry['pair'])
 
+# 3. Update (Modify)
+collection.update_one({'pair.name': 'New Pair'}, {'$set': {'pair.name': 'Updated Pair'}})
 
-
-@api_view(['post'])
-def get_data(request):
-    pairAddress_ = request.data
-    print(pairAddress_['body'])
-    pairAddress_=pairAddress_['body']
-    data = json.loads(json_string)
-    num_pairs = len(data[0]["pairs"])
-    price_native=''
-    price_usd=''
-    volume=[]
-    for i in (0,num_pairs-1):
-        if pairAddress_ == data[0]["pairs"][i]["pairAddress"]:
-            price_native=data[0]["pairs"][i]["priceNative"]
-            price_usd=data[0]["pairs"][i]["priceUsd"]
-            volume=data[0]["pairs"][i]["volume"]
-    print('price_usd',price_usd)
-    print('price_usd',price_native)
-    return JsonResponse({'price_usd':price_usd,'price_native':price_native,'volume':volume})
+# 4. Delete (Remove)
+delete_result = collection.delete_one({'pair.name': 'Updated Pair'})
+print("Deleted Count:", delete_result.deleted_count)
