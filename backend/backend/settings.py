356b6b572325ceb049f358c12cc60e489bb40487
ASHWANI TRIPATHI <tashwani475@gmail.com>
2024-03-22 21:51:29 +0530

    task A completed

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -4,17 +4,10 @@ from pathlib import Path
 
 
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = "django-insecure-4vin$fn--lf^q3b(jq*sf1$slp%hidw%)02x+13)qz23-*)3aw"
 
-# SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
 AUTH_USER_MODEL='account.User'
